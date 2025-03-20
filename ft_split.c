72de9873e4d594ef840f83a39a5b85230214db8a
Maminantenaina Harison Patrick Randriamananjara <maminran@c1r6s8.42antananarivo.mg>
2025-03-20 23:46:31 +0300

    libft

--- ft_split.c
+++ ft_split.c
@@ -5,65 +5,72 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: maminran <maminran@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/17 12:22:03 by maminran          #+#    #+#             */
-/*   Updated: 2025/03/17 12:31:17 by maminran         ###   ########.fr       */
+/*   Created: 2025/03/19 08:11:34 by maminran          #+#    #+#             */
+/*   Updated: 2025/03/19 09:37:24 by maminran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 static size_t	ft_find_c(const char *str, char c)
 {
 	size_t	count;
-	size_t	word;
 
 	count = 0;
-	word = 0;
 	while (*str)
 	{
-		if (*str != c && word == 0)
+		while (*str == c)
+			str++;
+		if (*str != '\0')
 		{
-			word = 1;
 			count++;
+			while (*str && *str != c)
+				str++;
 		}
-		else if (*str == c)
-			word = 0;
-		str++;
 	}
 	return (count);
 }
 
-static void	ft_free(char **tab, int letter)
+static void	ft_free(char **tab, size_t letter)
 {
-	while (letter >= 0)
-		free(tab[letter - 1]);
+	size_t	i;
+
+	i = 0;
+	while (i < letter)
+	{
+		free(tab[i]);
+		i++;
+	}
 	free(tab);
 }
 
 static char	*ft_create_word(const char *start, const char *end)
 {
-	char	*word;
 	size_t	len;
+	char	*word;
 	size_t	i;
 
+	i = 0;
 	len = end - start;
 	word = (char *)malloc(len + 1);
 	if (!word)
 		return (NULL);
-	i = 0;
 	while (i < len)
 	{
 		word[i] = start[i];
 		i++;
 	}
-	word[i] = '\0';
+	word[len] = '\0';
 	return (word);
 }
 
-static void	ft_split_helper(char **str, const char *s, char c, size_t *i)
+static void	ft_split_helper(char **str, const char *s, char c)
 {
 	const char	*begin;
+	size_t		i;
 
+	i = 0;
 	while (*s)
 	{
 		while (*s == c)
@@ -73,13 +80,13 @@ static void	ft_split_helper(char **str, const char *s, char c, size_t *i)
 			s++;
 		if (begin != s)
 		{
-			str[*i] = ft_create_word(begin, s);
-			if (!str[*i])
+			str[i] = ft_create_word(begin, s);
+			if (!str[i])
 			{
-				ft_free(str, *i);
+				ft_free(str, i);
 				return ;
 			}
-			(*i)++;
+			i++;
 		}
 	}
 }
@@ -87,17 +94,15 @@ static void	ft_split_helper(char **str, const char *s, char c, size_t *i)
 char	**ft_split(const char *s, char c)
 {
 	size_t	words;
-	size_t	i;
 	char	**str;
 
-	i = 0;
 	if (!s)
 		return (NULL);
 	words = ft_find_c(s, c);
 	str = (char **)malloc(sizeof(char *) * (words + 1));
 	if (!str)
 		return (NULL);
-	ft_split_helper(str, s, c, &i);
-	str[i] = NULL;
+	ft_split_helper(str, s, c);
+	str[words] = NULL;
 	return (str);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: maminran <maminran@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 15:03:58 by maminran          #+#    #+#             */
-/*   Updated: 2025/03/19 00:21:13 by maminran         ###   ########.fr       */
+/*   Updated: 2025/03/19 06:19:49 by maminran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		len = 0;
 	if ((ft_strlen(s) - start) < len)
 		len = ft_strlen(s) - start;
-	if (start <= ft_strlen(s))
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (i < len && s[start + i] != '\0')
 	{
-		str = malloc(sizeof(char) * (len + 1));
-		if (!str)
-			return (NULL);
-		while (i < len && s[start + i] != '\0')
-		{
-			str[i] = s[start + i];
-			i++;
-		}
+		str[i] = s[start + i];
+		i++;
 	}
-	else
-		str = malloc(sizeof(char));
-	str[i] = '\0';
+	str[len] = '\0';
 	return (str);
 }
