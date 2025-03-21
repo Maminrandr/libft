dc780e4b3d85271637bb375c3fd8aa621e817710
Maminantenaina Harison Patrick Randriamananjara <maminran@c1r8s2.42antananarivo.mg>
2025-03-21 09:07:44 +0300

    libft

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,38 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maminran <maminran@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/05 15:30:11 by maminran          #+#    #+#             */
-/*   Updated: 2025/03/21 08:06:51 by maminran         ###   ########.fr       */
+/*   Created: 2025/03/21 08:13:26 by maminran          #+#    #+#             */
+/*   Updated: 2025/03/21 09:06:36 by maminran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *nptr)
+t_list	*ft_lstnew(void *content)
 {
-	int	i;
-	int	sign;
-	int	result;
+	t_list	*new;
 
-	i = 0;
-	sign = 1;
-	result = 0;
-	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
-		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
-	{
-		if (nptr[i] == '-')
-			sign *= -1;
-		i++;
-	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
-	{
-		result = (result * 10) + (nptr[i] - '0');
-		i++;
-	}
-	return (result * sign);
+	new = malloc(sizeof(t_list));
+	new->content = content;
+	new->next = NULL;
+	return (new);
 }
